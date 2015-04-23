e7ee4bb8a3b82c2a6035d4920808801ae8f29cfc
DownSpiral <eric.d.wishart@gmail.com>
2015-04-23 00:26:42 -0700

    Add touch support
    Adds the ability to move players with touch screens.
    Movement is achieved by swiping in the direction that you want to move.

--- public/js/maze.js
+++ public/js/maze.js
@@ -41,3 +41,66 @@ $(window).off('keydown').on('keydown', function(e) {
   e.preventDefault();
   return false;
 });
+
+var ongoingTouches = new Array();
+var touch_threshold = 40;
+
+$(window).on("touchstart", function(evt) {
+  evt.preventDefault();
+  touches = evt.originalEvent.changedTouches;
+  for (var i=0; i < touches.length; i++) {
+    ongoingTouches.push(copyTouch(touches[i]));
+  }
+});
+
+$(window).on("touchmove", function(evt) {
+  evt.preventDefault();
+  var touches = evt.originalEvent.changedTouches;
+
+  for (var i=0; i < touches.length; i++) {
+    var idx = ongoingTouchIndexById(touches[i].identifier);
+    if(idx >= 0) {
+      var x_diff = ongoingTouches[idx].pageX - touches[i].pageX;
+      var y_diff = ongoingTouches[idx].pageY - touches[i].pageY;
+      if (Math.abs(x_diff) > touch_threshold || Math.abs(y_diff) > touch_threshold) {
+        ongoingTouches.splice(idx, 1, copyTouch(touches[i]));  // swap in the new touch record
+        if (x_diff > 0 && Math.abs(x_diff) > touch_threshold) {
+          maze.attempt_move("left");
+        } else if (x_diff < 0 && Math.abs(x_diff) > touch_threshold) {
+          maze.attempt_move("right");
+        } else if (y_diff > 0 && Math.abs(y_diff) > touch_threshold) {
+          maze.attempt_move("up");
+        } else if (y_diff < 0 && Math.abs(y_diff) > touch_threshold) {
+          maze.attempt_move("down");
+        }
+      }
+    }
+  }
+});
+
+$(window).on("touchend", function(evt) {
+  evt.preventDefault();
+  var touches = evt.originalEvent.changedTouches;
+
+  for (var i=0; i < touches.length; i++) {
+    var idx = ongoingTouchIndexById(touches[i].identifier);
+
+    if(idx >= 0) {
+      ongoingTouches.splice(idx, 1);  // remove it; we're done
+    }
+  }
+});
+
+function copyTouch(touch) {
+  return { identifier: touch.identifier, pageX: touch.pageX, pageY: touch.pageY };
+}
+
+function ongoingTouchIndexById(idToFind) {
+  for (var i=0; i < ongoingTouches.length; i++) {
+    var id = ongoingTouches[i].identifier;
+    if (id == idToFind) {
+      return i;
+    }
+  }
+  return -1;    // not found
+}

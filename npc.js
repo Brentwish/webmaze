94ee0baf3ffef821ee9d43353b5cf0f01d809633
brent <brent.wish@gmail.com>
2015-04-24 16:48:33 -0700

    Improved random bot logic

--- npc.js
+++ npc.js
@@ -26,32 +26,20 @@ npcObj.prototype.get_next_move = function(maze) {
   var facing_tile = maze.get_facing_tile(this.position, this.direction);
   var next_move;
 
-  if (facing_tile == this.position) {
-    next_move = surrounding_halls[i];
+  if (surrounding_halls.length > 2) {
+    next_move = _.sample(_.reject(surrounding_halls, function(t) {
+        return this.last_position.same_coords(t);
+    }, this));
   }
-  else if (facing_tile.is_hall()) {
-    if (surrounding_halls.length > 2) {
-      next_move = _.sample(_.reject(surrounding_halls, function(t) {
-        if (this.last_position.x == t.x && this.last_position.y == t.y) {
-          return t;
-        }
-      }, this));
-      next_move = surrounding_halls[i];
-    } else {
-    //if the tile in front of the bot is a hall, return it
-    next_move = {x: facing_tile.x, y: facing_tile.y};
-    }
+  else if (facing_tile != this.position && facing_tile.val == 1) {
+    next_move = facing_tile;
   }
   else {
-    //else choose a random surrounding hall and update its direction
     next_move = surrounding_halls[i];
   }
-
   this.direction = this.get_direction(next_move);
   this.last_position = this.position;
   this.position = next_move;
   return next_move;
-
-  //else if the tile in the direction of the start is a hall, return it
 }
 exports.npcObj = npcObj;

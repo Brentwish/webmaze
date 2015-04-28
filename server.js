1493808b56834eb2c58b82115e6595003cb5f3d5
DownSpiral <eric.d.wishart@gmail.com>
2015-04-28 12:50:15 -0700

    Refactor server to use game rooms
    Adds a list of game rooms that are filled with players as they join.
    Removes game rooms that are empty/stale at a set interval.

--- server.js
+++ server.js
@@ -2,31 +2,23 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var maze_gen = require('./maze_gen.js');
-var npc = require('./npc.js');
 var game = require('./game.js');
+var game_room = require('./game_room.js');
+var randomstring = require("randomstring");
 var _ = require('./public/js/underscore-min.js');
 
 var server_port = (process.env.PORT || 3000);
+var game_rooms = [];
 
-var game_settings = {
-  maze_size_x: 8,
-  maze_size_y: 8,
-  num_teleporters: 6,
-  num_npcs: 4,
-  game_tick_length: 20
-};
-
-var the_game = new game.game(game_settings);
-
-function game_update() {
-  var update = the_game.game_tick();
-  if (_.any(update, function(v) { return !_.isEmpty(v); })) {
-    io.emit('game_update', update);
-  }
-}
-
-setInterval(game_update, the_game.game_tick_length);
+setInterval(function() {
+  game_rooms = _.reject(game_rooms, function(room) {
+    var should_remove = room.should_be_removed();
+    if (should_remove) {
+      console.log("removing room: " + room.id + " from room pool");
+    }
+    return should_remove;
+  });
+}, 5000);
 
 app.use("/public", express.static(__dirname + '/public'));
 
@@ -39,37 +31,15 @@ app.get('/maze/', function(req, res) {
 });
 
 io.on('connection', function(socket) {
-  //Add the player to the game
-  the_game.add_player({ id: socket.id });
-
-  //Send the maze to the new player
-  socket.emit('maze_data', the_game.to_data_hash(socket.id));
-  
-  //Let everyone else know about the new player
-  io.emit('player_update', the_game.players[socket.id]);
-
-
-  socket.on('coord_update', function(player_coord) {
-    var updates = the_game.handle_player_update(player_coord, socket.id);
-    if (!_.isEmpty(updates)) {
-      io.emit('player_update', updates["player_data"]);
-    }
-
-    if (the_game.is_over) {
-      the_game.reset_game();
-      io.emit('maze_data', the_game.to_data_hash(socket.id));
-    }
-  });
-
-  console.log('a user connected');
-  console.log('Number of players: ' + Object.keys(the_game.players).length);
-  io.emit('user_connect', {data : 'a user connected'});
-  socket.on('disconnect', function(){
-    delete the_game.players[socket.id];
-    console.log('user disconnected');
-    console.log('Number of players: ' + Object.keys(the_game.players).length);
-    io.emit('player_disconnect', {id: socket.id});
-  });
+  console.log("player: " + socket.id + " is looking for room");
+  var room = _.find(game_rooms, function(room) { return room.has_room(); });
+  if (!room) {
+    console.log("no open rooms. making a new one");
+    room = new game_room.game_room({ name: "harg", io: io, id: randomstring.generate() });
+    game_rooms.push(room);
+  }
+  console.log("calling add player for: " + socket.id);
+  room.join(socket);
 });
 
 io.on('connection', function(socket){
